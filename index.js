02d752eb70381982d01edb000587807d3f756bad
Saif Al-Din Samy <SeifOwleN@proton.me>
2023-09-20 06:52:21 +0300

    First commit for deployment test

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+
+morgan.token("Data", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :Data")
+);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/info", (request, response) => {
+  const date = new Date().toLocaleString();
+  const local = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people </p></br><p>${date} ${local}</p>`
+  );
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((p) => p.id == id);
+  if (person) {
+    return res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((p) => p.id != id);
+
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const person = req.body;
+  if (!person.name) {
+    return res.status(400).json({
+      error: "name missing",
+    });
+  } else if (!person.number) {
+    return res.status(400).json({
+      error: "number missing",
+    });
+  } else if (persons.find((p) => person.name == p.name)) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  person.id = Math.floor(Math.random() * 100000);
+
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
