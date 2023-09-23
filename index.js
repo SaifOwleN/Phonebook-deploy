cce0bf04ea7c10640fee4bba2e4b69ad883fb414
Saif Al-Din Samy <SeifOwleN@proton.me>
2023-09-24 00:01:07 +0300

    uibuild

--- index.js
+++ index.js
@@ -10,93 +10,93 @@ app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
 
-morgan.token("Data", function (req, res) {
-  return JSON.stringify(req.body);
+morgan.token("Data", function (request, ) {
+    return JSON.stringify(request.body);
 });
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :Data")
+    morgan(":method :url :status :res[content-length] - :response-time ms :Data")
 );
 
 const ErrorHandler = (error, request, response, next) => {
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  }
-  next(error);
+    if (error.name === "CastError") {
+        return response.status(400).send({ error: "malformatted id" });
+    } else if (error.name === "ValidationError") {
+        return response.status(400).json({ error: error.message });
+    }
+    next(error);
 };
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
+    response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.get("/info", (request, response) => {
-  const date = new Date().toLocaleString();
-  const local = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const date = new Date().toLocaleString();
+    const local = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-  response.send(
-    `<p>Phonebook has info for ${Entry.length} people </p></br><p>${date} ${local}</p>`
-  );
+    response.send(
+        `<p>Phonebook has info for ${Entry.length} people </p></br><p>${date} ${local}</p>`
+    );
 });
 
 app.get("/api/persons", (req, res) => {
-  Entry.find({}).then((entry) => res.json(entry));
+    Entry.find({}).then((entry) => res.json(entry));
 });
 
 app.get("/api/persons/:id", (req, res, next) => {
-  Entry.findById(req.params.id)
-    .then((entry) => {
-      if (entry) {
-        return res.json(entry);
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
+    Entry.findById(req.params.id)
+        .then((entry) => {
+            if (entry) {
+                return res.json(entry);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (req, res, next) => {
-  Entry.findByIdAndRemove(req.params.id)
-    .then((r) => {
-      res.status(204).end();
-    })
-    .catch((error) => next(error));
+    Entry.findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.status(204).end();
+        })
+        .catch((error) => next(error));
 });
 
 app.post("/api/persons", (req, res, next) => {
-  const entry = new Entry({
-    name: req.body.name,
-    number: req.body.number,
-  });
-  if (!entry.name) {
-    return res.status(400).json({
-      error: "name missing",
+    const entry = new Entry({
+        name: req.body.name,
+        number: req.body.number,
     });
-  } else if (!entry.number) {
-    return res.status(400).json({
-      error: "number missing",
-    });
-  }
+    if (!entry.name) {
+        return res.status(400).json({
+            error: "name missing",
+        });
+    } else if (!entry.number) {
+        return res.status(400).json({
+            error: "number missing",
+        });
+    }
 
-  entry
-    .save()
-    .then((entry) => res.json(entry))
-    .catch((error) => next(error));
+    entry
+        .save()
+        .then((entry) => res.json(entry))
+        .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (req, res, next) => {
-  const entry = {
-    name: req.body.name,
-    number: req.body.number,
-  };
-  console.log("entry", entry);
+    const entry = {
+        name: req.body.name,
+        number: req.body.number,
+    };
+    console.log("entry", entry);
 
-  Entry.findByIdAndUpdate(req.params.id, entry)
-    .then((updatedPerson) => {
-      res.json(updatedPerson);
-    })
-    .catch((error) => next(error));
+    Entry.findByIdAndUpdate(req.params.id, entry)
+        .then((updatedPerson) => {
+            res.json(updatedPerson);
+        })
+        .catch((error) => next(error));
 });
 
 app.use(ErrorHandler);
@@ -104,5 +104,5 @@ app.use(unknownEndpoint);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
--- mongo.js
+++ mongo.js
@@ -2,8 +2,8 @@ const { ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+    console.log("give password as argument");
+    process.exit(1);
 }
 
 const password = process.argv[2];
@@ -14,24 +14,24 @@ mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
 const entrySchema = new mongoose.Schema({
-  name: String,
-  number: String,
+    name: String,
+    number: String,
 });
 
 const Entry = mongoose.model("Entry", entrySchema);
 
 const entry = new Entry({
-  name: process.argv[3],
-  number: process.argv[4],
+    name: process.argv[3],
+    number: process.argv[4],
 });
 
 if (process.argv.length == 3) {
-  Entry.find({}).then((result) => {
-    console.log(result);
-    mongoose.connection.close();
-  });
+    Entry.find({}).then((result) => {
+        console.log(result);
+        mongoose.connection.close();
+    });
 }
 entry.save().then((result) => {
-  console.log(result);
-  mongoose.connection.close();
+    console.log(result);
+    mongoose.connection.close();
 });
