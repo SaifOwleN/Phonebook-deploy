d106ea4650deb53bbfdcc68bf298337d4fae07aa
Saif Al-Din Samy <SeifOwleN@proton.me>
2023-09-24 00:10:52 +0300

    deleted mongo.js

--- models/entries.js
+++ models/entries.js
@@ -16,22 +16,22 @@ mongoose
     });
 
 const entrySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3,
-  },
-  number: {
-    type: String,
-    minLength: 8,
-  },
+    name: {
+        type: String,
+        minLength: 3,
+    },
+    number: {
+        type: String,
+        minLength: 8,
+    },
 });
 
 entrySchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
 });
 
 module.exports = mongoose.model("Entry", entrySchema);
